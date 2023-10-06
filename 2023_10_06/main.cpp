5118143eae870e105ec2b8f43526760021448f05
DanieleDituri <daniele.dituri@gmail.com>
2023-10-06 12:00:19 +0200

    2023_10_06

---
+++ 2023_10_06/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "ops.h"
+
+int main(){
+
+    std::cout << "Hello world" << std::endl;
+
+    std::string s;
+
+    std::cout << "Insert Message ";
+    std::cin >> s;
+    std::cout << "The message is " << s << std::endl;
+
+    //stampa();
+
+    return 0;
+}
