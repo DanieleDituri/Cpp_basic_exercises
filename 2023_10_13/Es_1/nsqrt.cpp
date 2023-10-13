1fea8614908df152f92efc9704ea757ee114cdb1
DanieleDituri <daniele.dituri@gmail.com>
2023-10-13 13:17:49 +0200

    es1

--- 2023_10_13/Es_1/nsqrt.cpp
+++ 2023_10_13/Es_1/nsqrt.cpp
@@ -1,11 +1,16 @@
-#include <nsqrt.h>
+#include "nsqrt.h"
 #include <cmath>
+#include <cassert>
 
 double nsqrt(double x, double epsilon){
+
+    assert(x > 0);
+    assert(epsilon > 0);
+
     double s = x/2;
 
     while (std::fabs(s * s -x) >= epsilon){
-        s = (s * s + x) / (2 * s)
+        s = (s * s + x) / (2 * s);
     }
 
     return s;
--- 2023_10_13/Es_1/test_nsqrt.cpp
+++ 2023_10_13/Es_1/test_nsqrt.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
+#include <cmath>
 #include "nsqrt.h"
+
+int main(){
+    double x = 0.0;
+    std::cout << "Dare un valore reale positivo: ";
+    std::cin >> x;
+
+    double radix = nsqrt(x, 1e-3);
+
+    std::cout << "Il valore di nsqrt(x) è: " << radix << std::endl;
+    std::cout << "Il valore di std::sqrt(x) è: " << std::sqrt(x) << std::endl;
+
+    return 0;
+}
