b73dec6bf933adb9f58e72dc77a38bf3dfa3fa5c
DanieleDituri <daniele.dituri@gmail.com>
2023-10-17 10:29:58 +0200

    es3

--- 2023_10_13/ES_3/date.cpp
+++ 2023_10_13/ES_3/date.cpp
@@ -28,28 +28,82 @@ int date_difference(int year1, int month1, int day1, int year2, int month2, int 
             return -1;
         }
     }
-
-    for (int i = year1; i < year2 - 1; i++){
-        if (i % 4 == 0){
+    int i = year1;
+    if (month1 >= month2)
+    {
+        i++;
+    }
+    for (i; i < year2; ++i)
+    {
+        if (i % 4 == 0)
+        {
             diff += 366;
         }
-        else{
+        else
+        {
             diff += 365;
         }
     }
-    for (int i = month1; i < month2; i++){
-        if(i == 4 || i == 6 || i == 9 || i == 11){
-            diff += 30;
+    int j = month2 - month1;
+    i = month1;
+    if(day1 >= day2){
+        i++;
+    }
+    if (j < 0)
+    {
+        j += 11;
+    }
+    while (j != 0)
+    {
+        if (i > 12)
+        {
+            i -= 12;
+            year1++;
         }
-        else if(i == 2){
-            diff += 28;
+        if (i == 4 || i == 6 || i == 9 || i == 11)
+        {
+            diff += 30;
+            i++;
+        }
+        else if (i == 2)
+        {
+            if (year1 % 4 == 0)
+            {
+                diff += 29;
+                i++;
+            }
+            else
+            {
+                diff += 28;
+                i++;
+            }
         }
         else{
             diff += 31;
+            i++;
         }
+        j--;
     }
-    for (int i = day1; i < day2; i++){
-        diff++;
+    if(day1 < day2){
+        diff += day2 - day1;
     }
+    else{
+        if(month1 == 4 || month1 == 6 || month1 == 9 || month1 == 11){
+            diff += 30 - day1;
+        }
+        else if (month1 == 2){
+            if(year1 % 4 == 0){
+                diff += 29 - day1;
+            }
+            else{
+                diff += 28 - day1;
+            }
+        }
+        else{
+            diff += 31 - day1;
+        }
+        diff += day2;
+    }
+
     return diff;
 }
