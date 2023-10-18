759ecd91be62e8a369e5589511ea4410bf48e6a4
DanieleDituri <daniele.dituri@gmail.com>
2023-10-18 18:11:44 +0200

    2023_10_18 v2

--- 2023_10_18/main.cpp
+++ 2023_10_18/main.cpp
@@ -25,6 +25,32 @@ void h()
     p = nullptr;
 }
 
+void m()
+{
+    int **p = nullptr;
+
+    p = new int *;
+
+    delete p;
+    p = nullptr;
+}
+
+void a()
+{
+    int *p = nullptr;
+    int n = 90;
+
+    p = new int[n];
+
+    // uso p
+
+    *(p + 1) = 9;
+    p = p + 1;
+
+    delte[] p; // delete array
+    p = nullptr;
+}
+
 int main(int argc, char *argv[])
 {
     int j = 0; // variabli/dati allocati sullo stack
